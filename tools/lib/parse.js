a21719824cfb7a951fb67d14a1b2c2b1c40aa5e2
Herman Chau <hchau@stanford.edu>
2016-01-24 10:40:47 -0800

    Modularize script parsing code. Ready to add support for auto line breaks and indentation in script.

--- tools/lib/parse.js
+++ tools/lib/parse.js
@@ -1,56 +1,69 @@
-function initialParse(english) {
+// Perform macro substitution (non-recursive)
+function substituteMacros(english) {
+  var strings = english.split("$");
+  if (strings.length % 2 == 0) {
+    console.log("Missing terminating '$' in text '" + english + "'");
+    return;
+  }
+  for (var ii = 1; ii < strings.length; ii += 2) {
+    if (!glossary[strings[ii]]) {
+      console.log ("Glossary name '" + strings[ii] + "' doesn't exist!");
+      return;
+    }
+    strings[ii] = glossary[strings[ii]];
+  }
+  return strings.join("");
+}
+
+// Auto line break and indentation for script dialogue. Call after initial parse.
+function preprocessScript(bytes) {
+  var parts = bytes.split(0x40); // Split at @ symbol.
+  if (parts.length != 2) return bytes;
+  var lines = parts[1].split(0x80); 
+}
+
+// Processes escaped hex values and alignment characters.
+function processSpecialCharacters(english) {
   var initial_parsed = [];
   for (var ii = 0; ii < english.length; ii++) {
     var c = english[ii];
     if (c == '\\') {
       // Insert a 1 byte hexadecimal value (e.g. '\8e').
-      initial_parsed.push(parseInt(english.substring(ii+1, ii+3), 16));
+      initial_parsed.push(String.fromCharCode(parseInt(english.substring(ii+1, ii+3), 16)));
       ii += 2;
     } else if (c == '\n') {
       // Ignore newlines.
     } else if (c == '|') {
       // Aligns the next character at a 2 byte boundary, padding with 0's.
-      initial_parsed.push(-1);
+      initial_parsed.push(String.fromCharCode(0x101));
     } else if (c == '`') {
       // Resets the alignment.
-      initial_parsed.push(-2);
-    } else if (c == '$') {
-      // Glossary substitution.
-      var end = english.indexOf('$', ii+1);
-      if (end == -1) {
-        console.log("Missing terminating '$' in text '" + english + "'");
-        return;
-      }
-      var name = english.substring(ii+1, end);
-      if (!glossary[name]) {
-        console.log("Glossary name '"+ name +"' doesn't exist!")
-        return;
-      }
-      initial_parsed = initial_parsed.concat(initialParse(glossary[name]));
-      ii += name.length + 1;
+      initial_parsed.push(String.fromCharCode(0x102));
     } else {
-      initial_parsed.push(c.charCodeAt(0));
+      initial_parsed.push(c);
     }
   }
-  return initial_parsed;
+  return initial_parsed.join("");
 }
+
 function parseEnglish(english) {
-  var initial_parsed = [0x1F, 0x00].concat(initialParse(english), [-1, 0xFF, 0xFF]);
+  english = String.fromCharCode(0x1F, 0x00) + english + String.fromCharCode(0x101, 0xFF, 0xFF);
   var ii = 0;
   var alignment = 0;
   var parsed = [];
-  initial_parsed.forEach(function (n) {
-    if (n == -1) {
+  for (var ii = 0; ii < english.length; ii++) {
+    var n = english.charCodeAt(ii);
+    if (n == 0x101) {
       if (alignment == 1) {
         parsed.push(0x00);
         alignment = 0;
       }
-    } else if (n == -2) {
+    } else if (n == 0x102) {
       alignment = 0;
     } else {
       parsed.push(n);
       alignment ^= 1;
     }
-  })
+  }
   return parsed;
 }
